872fc6c6f417f1b4bda642b338c68d5b0335862f
Gedion <gedion.basson@gmail.com>
2026-04-24 09:06:09 +0200

    Section B Question1  finished

---
+++ SectionB-Question1/Resident.cs
@@ -0,0 +1,10 @@
+﻿namespace SectionB_Question1
+{
+    public class Resident
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string AccountNumber { get; set; }
+        public double MonthlyUtilityUsage { get; set; }
+    }
+}
